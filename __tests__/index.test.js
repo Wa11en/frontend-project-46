5e131767b81c1d9e5621b7ef5728cbbfdc096890
Valentin Grishin <kolumbus59@gmail.com>
2024-01-30 15:29:17 +0300

    fix index.js; edit tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,10 +10,10 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filepath) => path.join(__dirname, '..', '__fixtures__', filepath);
 const readFile = (filepath) => readFileSync(getFixturePath(filepath), 'utf-8');
 
-const expectedStylish = readFile('expected.txt');
+const expected = readFile('expected.txt');
 
 test.each(['json', 'yaml', 'yml'])('genDiff-test', (extension) => {
   const fileBefore = `__fixtures__/file1.${extension}`;
   const fileAfter = `__fixtures__/file2.${extension}`;
-  expect(genDiff(fileBefore, fileAfter)).toEqual(expectedStylish);
+  expect(genDiff(fileBefore, fileAfter)).toEqual(expected);
 });
--- src/index.js
+++ src/index.js
@@ -13,15 +13,15 @@ const getDifferences = (data1, data2) => {
   const sortedKeys = _.sortBy(keys);
   const diff = sortedKeys.map((key) => {
     if (!_.has(data1, key)) {
-      return ` + ${key}: ${data2[key]}`;
+      return `  + ${key}: ${(data2[key])}`;
     }
     if (!_.has(data2, key)) {
-      return ` - ${key}: ${data1[key]}`;
+      return `  - ${key}: ${(data1[key])}`;
     }
     if (data1[key] === data2[key]) {
-      return `   ${key}: ${data1[key]}`;
+      return `    ${key}: ${(data1[key])}`;
     }
-    return ` - ${key}: ${data1[key]}\n + ${key}: ${data2[key]}`;
+    return `  - ${key}: ${(data1[key])}\n  + ${key}: ${(data2[key])}`;
   });
 
   return `{\n${diff.join('\n')}\n}`;
