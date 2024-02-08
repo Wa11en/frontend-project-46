f912772c4e760b7f3ee8581d5cd738cc9a610790
Valentin Grishin <kolumbus59@gmail.com>
2024-02-08 13:19:31 +0300

    edit buildTree

--- src/buildTree.js
+++ src/buildTree.js
@@ -8,17 +8,20 @@ const makeTree = (obj1, obj2) => {
     if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
       return { key, children: makeTree(obj1[key], obj2[key]), status: 'nested' };
     }
-    if (!Object.hasOwn(obj1, key)) {
+    if (!(key in obj1)) {
       return { key, value: obj2[key], status: 'added' };
     }
-    if (!Object.hasOwn(obj2, key)) {
+    if (!(key in obj2)) {
       return { key, value: obj1[key], status: 'removed' };
     }
     if (obj1[key] === obj2[key]) {
       return { key, value: obj1[key], status: 'unupdated' };
     }
     return {
-      key, oldValue: obj1[key], newValue: obj2[key], status: 'updated',
+      key,
+      oldValue: obj1[key],
+      newValue: obj2[key],
+      status: 'updated',
     };
   });
 };
